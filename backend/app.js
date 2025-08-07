18966e08fbe45830299d620378ddcec29822edf1
ARYANSH Chauhan <146801011+aryansh2616@users.noreply.github.com>
2025-08-07 11:41:26 +0530

    minor 2 changes

--- backend/app.js
+++ backend/app.js
@@ -56,6 +56,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
+console.log(PORT);
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server is listening on port ${PORT}`);
 });
